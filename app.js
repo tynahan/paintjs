a59980985b3aacf42c7fe3068c1633637cb6a567
Youngwoo Nam <tynahan@gmail.com>
2020-09-12 22:57:56 +0900

    #2.0 Event - Canvas 2d Context

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -13,6 +20,13 @@ function startPaint() {
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
